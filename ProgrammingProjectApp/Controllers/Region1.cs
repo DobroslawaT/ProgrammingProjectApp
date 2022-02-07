a62d5437d5d9816932fd245609cab12fcdd738b6
Dobrosława Taras <w64608@student.wsiz.edu.pl>
2022-02-07 19:37:54 +0100

    Controllers

---
+++ ProgrammingProjectApp/Controllers/Region1.cs
@@ -0,0 +1,37 @@
+﻿using ProgrammingProjectApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingProjectApp.Controllers
+{
+    public class Region1Controller : Controller
+    {
+        private readonly ILogger<Region1Controller> _logger;
+
+        public Region1Controller(ILogger<Region1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProgrammingProjectApp/Controllers/Region2.cs
@@ -0,0 +1,37 @@
+﻿using ProgrammingProjectApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingProjectApp.Controllers
+{
+    public class Region2Controller : Controller
+    {
+        private readonly ILogger<Region2Controller> _logger;
+
+        public Region2Controller(ILogger<Region2Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
