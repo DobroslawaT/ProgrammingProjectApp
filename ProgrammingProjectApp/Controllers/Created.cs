950bcbe5f2c325eaee5da4bebb444d3ee32874ae
Dobrosława Taras <w64608@student.wsiz.edu.pl>
2022-02-09 21:22:29 +0100

    .

---
+++ ProgrammingProjectApp/Controllers/Created.cs
@@ -0,0 +1,37 @@
+﻿using ProgrammingProjectApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingProjectApp.Controllers
+{
+    public class Created : Controller
+    {
+        private readonly ILogger<Created> _logger;
+
+        public Created(ILogger<Created> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
